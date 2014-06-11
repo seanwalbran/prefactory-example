209b61226c12ff1d62606c7ae3f0a60c7266ea3d
Sean Walbran <swalbran@socialcast.com>
2014-06-11 07:36:50 -0500

    convert to rspec3 syntax via transpec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-describe User do
+describe User, :type => :model do
 
   set!(:user)
 
   context 'a new user has no friends' do
 
-    it { user.friends.count.should == 0 }
+    it { expect(user.friends.count).to eq(0) }
 
     context 'with a friend' do
 
@@ -15,25 +15,25 @@ describe User do
 
       before(:all) { user.add_friend(friend) }
 
-      it { user.friends.count.should == 1 }
+      it { expect(user.friends.count).to eq(1) }
 
       it "allows removing the friend" do
         expect { user.remove_friend(friend) }.to_not raise_error
-        user.friends.count.should == 0
+        expect(user.friends.count).to eq(0)
       end
 
       it "disallows adding the same friend again" do
         expect { user.add_friend(friend) }.to raise_error
-        user.friends.count.should == 1
+        expect(user.friends.count).to eq(1)
       end
 
       it "allows adding a different friend" do
         expect { user.add_friend(other_friend) }.to_not raise_error
-        user.friends.count.should == 2
+        expect(user.friends.count).to eq(2)
       end
 
-      it { user.friends.should include friend }
-      it { user.friends.should_not include other_friend }
+      it { expect(user.friends).to include friend }
+      it { expect(user.friends).not_to include other_friend }
     end
   end
 end
